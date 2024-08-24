5caf6f02a828d76d120884f7bc1dc12563c67ae5
TheAMIZZguy <AndresZepeda137@gmail.com>
2024-08-23 22:59:47 -0700

    Finished Game.py v2

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ The important capabilities are:
 """
 class Game:
 
-    def __init__(self, board: list[list[int]] = None):
+    def __init__(self, board: list[list[int]] = None, current_player: int = None, possible_moves : dict[int] = None):
         if board is None:
             self.current_player = 1
             self.board = [[0,0,0,0,0,0,0],
@@ -40,10 +40,16 @@ class Game:
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0]]
-            self.possible_moves = [[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6]]
+            self.possible_moves = {0: 5, 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 5}
         else:
             self.board = board
-            self.current_player, self.possible_moves = self.SetupBoard(self.board)
+            if current_player is not None and possible_moves is not None:
+                self.current_player = current_player
+                self.possible_moves = possible_moves
+            else:
+                self.current_player, self.possible_moves = self.SetupBoard(self.board)
+
+        self.other_player = 2 if self.current_player == 1 else 1
 
 
     """
@@ -62,7 +68,7 @@ class Game:
     @staticmethod
     def SetupBoard(board: list[list[int]]):
         move_count = 0
-        possible_moves = []
+        possible_moves = {}
 
         for col in range(len(board[0])):
             for row in reversed(range(len(board))):
@@ -71,7 +77,7 @@ class Game:
                     move_count += 1
                 if value == 0:
                     if row == len(board) - 1 or board[row + 1][col] != 0:
-                        possible_moves.append([row, col])
+                        possible_moves[col] = row
                     break
 
         return (move_count % 2) + 1, possible_moves
@@ -88,13 +94,12 @@ class Game:
         mirrored_board_tuple = tuple(tuple(row) for row in mirrored_board)
         return hash(mirrored_board_tuple)
 
-
     """
     Makes the move from the player and updates the game accordingly
     """
-    def MakeMove(self, move):
+    def MakeMove(self, col):
         # Make the move
-        self.board[move[0]][move[1]] = self.current_player
+        self.board[col][self.possible_moves[col]] = self.current_player
 
         # Change the player
         if self.current_player == 1:
@@ -103,76 +108,67 @@ class Game:
             self.current_player = 1
 
         # Update possible moves
-        def UpdatePossibleMoves(self, col):
-            if self.possible_moves[col] > 0:
-                self.possible_moves[col] -= 1  # Move up by 1 due to gravity
-            else:
-                self.possible_moves.pop(col)  # Remove if it's already at the top
-        # TODO change the rest of stuff with possible moves so I can use this logic
-
-
-        #The history of the play
-        newHistory = (state.playHistory[:])
-        #add the current play to history
-        newHistory.append(move) 
-        #make this new state have the same board to edit.
-
-        newBoard = [row[:] for row in state.board]
-        if newBoard[move[0]][move[1]] == 3: #making sure its a possible move
-            newBoard[move[0]][move[1]] = state.player #make the move as the player
-            if move[0] > 0: #if its not at the top...
-                newBoard[move[0]-1][move[1]] = 3 #make place above possible
-        #print("PLAYER:", player)
-        newPlayer = self.InvertPlayer(state.player) #invert the player
-        #print("PLAYER@:", newPlayer)
-        return State(newHistory, newBoard, newPlayer) #new state
-    
-    #inverts between player 1 and 2, self explanitory
-    def InvertPlayer(self, player): 
-        tempPlayer = player
-        if tempPlayer == 1:
-            tempPlayer = 2
+        if self.possible_moves[col] > 0:
+            self.possible_moves[col] -= 1  # Move up by 1 due to gravity
         else:
-            tempPlayer = 1
-        return tempPlayer
+            self.possible_moves.pop(col)  # Remove if it's already at the top
 
     def PrintBoard(self):
         for row in self.board:
             print(row)
         print("-")
 
-     
-    #Simulates a random game from a state and current player
-    def RandomGame(self, state, player):
-        randomPlayer = player        
-        winner = -1
-        while winner == -1:
-            #self.PrintBoard(randomBoard)
-            move = random.choice(self.PossibleMoves(state))
-            state.board = self.MakeMove(state, move[0], move[1], randomPlayer)
-            winner = self.Winner(state.board)
-            if self.PossibleMoves(state.board) == []:
-                winner = 0
-            if randomPlayer == 1:
-                randomPlayer = 2
-            else:
-                randomPlayer = 1
-        #print("The winner is", winner)
-        return winner
-    
-    #checks for winner, returns the int of the player who won, 0 for draw, and -1 for no winner
-    def Winner(self, state):
-        board = state.board
-        
-        #if the top row is full of 1's and 2's it means its full and therefore tie
-        if (3 in board[0]) or (0 in board[0]):
-          pass
-        else:
-            return 0
-            
-        
+    """
+    Plays a random game from current position
+    """
+    def RandomGame(self):
+        hasWon = False
+        while not hasWon:
+            if len(self.possible_moves.keys()) == 0:
+                return 0
+            col = random.choice(self.possible_moves.keys())
+            row = self.possible_moves[col]
+            self.MakeMove(col)
+            hasWon = self.CheckWin(row, col)
+        return self.other_player
+
+    """
+    The fast version of checking to see if a move leads to a win
+    """
+    def CheckWin(self, row, col):
+        def count_in_direction(delta_row, delta_col):
+            count = 0
+            r, c = row + delta_row, col + delta_col
+            while 0 <= r < len(self.board) and 0 <= c < len(self.board[0]) and self.board[r][c] == self.other_player:
+                count += 1
+                r += delta_row
+                c += delta_col
+            return count
+
+        # Check all four directions
+        directions = [
+            (0, 1),  # Horizontal right
+            (1, 0),  # Vertical down
+            (1, 1),  # Diagonal down-right
+            (1, -1)  # Diagonal down-left
+        ]
+
+        for delta_row, delta_col in directions:
+            count = 1  # Start with the current piece
+            count += count_in_direction(delta_row, delta_col)  # Check in the positive direction
+            count += count_in_direction(-delta_row, -delta_col)  # Check in the negative direction
+            if count >= 4:
+                return True  # Win found
+
+        return False  # No win found
+
+    """
+    checks for winner, returns the int of the player who won, 0 for draw, and -1 for no winner
+    """
+    def Winner(self):
+
         #Horizontal Win
-        for row in board:
+        for row in self.board:
             for num in range(0,4):#so it doesnt go out of index range
                 #print(num)cc
                 if row[num] == row[num+1] == row[num+2] == row[num+3] and not (row[num] == 3 or row[num] == 0):
@@ -183,22 +179,22 @@ class Game:
         for col in range(0,7): #using range so its based on the index
             for row in range(0,3): #less rows
                 #print(num)
-                if board[row][col] == board[row+1][col] == board[row+2][col] == board[row+3][col] and not (board[row][col] == 3 or board[row][col] == 0):
-                    return board[row][col]
+                if self.board[row][col] == self.board[row+1][col] == self.board[row+2][col] == self.board[row+3][col] and not (self.board[row][col] == 3 or self.board[row][col] == 0):
+                    return self.board[row][col]
                 
         #Left diagonal win
         for row in range(0,3): #using range so its based on the index
             for col in range(0,4): #the rest is the same
                 #print(num)
-                if board[row][col] == board[row+1][col+1] == board[row+2][col+2] == board[row+3][col+3] and not (board[row][col] == 3 or board[row][col] == 0):
-                    return board[row][col] 
+                if self.board[row][col] == self.board[row+1][col+1] == self.board[row+2][col+2] == self.board[row+3][col+3] and not (self.board[row][col] == 3 or self.board[row][col] == 0):
+                    return self.board[row][col]
                 
         #Right diagonal win
         for row in range(0,3): #using range so its based on the index
             for col in [6,5,4,3]: #it needs to loop from the right
                 #print(num)
-                if board[row][col] == board[row+1][col-1] == board[row+2][col-2] == board[row+3][col-3] and not (board[row][col] == 3 or board[row][col] == 0):
-                    return board[row][col]
+                if self.board[row][col] == self.board[row+1][col-1] == self.board[row+2][col-2] == self.board[row+3][col-3] and not (self.board[row][col] == 3 or self.board[row][col] == 0):
+                    return self.board[row][col]
         
         #-1 means that there is no winner... yet
         return -1
